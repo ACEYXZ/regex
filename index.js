e2d17d4249d06abb87d731d1ca6250545595fee7
Joao Pedro Nicacio <jpn.athademo@gmail.com>
2025-06-10 22:42:21 -0300

    ultimo commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+function code1 (){ 
+    const senha = "O INVASOR OCULTO DECIFROU O FIREWALL SAGRADO"
+    const input2 = document.getElementById("input2").value;
+    if (input2 == senha){ 
+        window.location.href = "trava.html"
+    }else{
+        alert("Senha incorreta, tente novamente.");
+    } 
+}
+
+
+function code2 (){ 
+    const senha = "Gs@5&%G5h72!iJc6$#+P"
+    const input = document.getElementById("input").value;
+    if (input == senha){ 
+        window.location.href = "pt2.html"
+    }else{
+        alert("Senha incorreta, tente novamente.");
+    } 
+}
